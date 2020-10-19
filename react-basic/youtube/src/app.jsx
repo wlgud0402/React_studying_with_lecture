9c445dfcf93652a014f272915c16e0997d9cec98
wlgud0402 <wlgudrlgus@naver.com>
2020-10-20 00:34:47 +0900

    add search bar

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import "./app.css";
+import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
+import SearchHeader from "./components/search_header/search_header";
 
 function App() {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
+    //포스트맨의 youtube api 사용 코드
     const requestOptions = {
       method: "GET",
       redirect: "follow",
@@ -19,7 +21,12 @@ function App() {
       .then((result) => setVideos(result.items))
       .catch((error) => console.log("error", error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={StyleSheet.app}>
+      <SearchHeader />
+      <VideoList videos={videos} />;
+    </div>
+  );
 }
 
 export default App;
