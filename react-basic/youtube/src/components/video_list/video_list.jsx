b73bae132d6c9e87277b36179f39eeb9cc1695a0
wlgud0402 <wlgudrlgus@naver.com>
2020-10-19 23:02:39 +0900

    add css

--- react-basic/youtube/src/components/video_list/video_list.jsx
+++ react-basic/youtube/src/components/video_list/video_list.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import VideoItem from '../video_item/video_item';
+import React from "react";
+import VideoItem from "../video_item/video_item";
+import styles from "./video_list.module.css";
 
-const VideoList = props => (
-  <ul>
-    {props.videos.map(video => (
+const VideoList = (props) => (
+  <ul className={styles.videos}>
+    {props.videos.map((video) => (
       <VideoItem key={video.id} video={video} />
     ))}
   </ul>
