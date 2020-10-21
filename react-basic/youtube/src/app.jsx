d8f08cdd6147b515765f3395d45facab637e6d5f
wlgud0402 <wlgudrlgus@naver.com>
2020-10-21 21:40:28 +0900

    refactoring and search bar

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -1,30 +1,25 @@
-import React, { useEffect, useState } from "react";
-import styles from "./app.module.css";
-import VideoList from "./components/video_list/video_list";
-import SearchHeader from "./components/search_header/search_header";
+import React, { useEffect, useState } from 'react';
+import styles from './app.module.css';
+import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
+  };
 
   useEffect(() => {
-    //포스트맨의 youtube api 사용 코드
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAtrSZkopq--QXlpEYQ5SrM9Kg5TZlZMl0",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
   }, []);
   return (
-    <div className={StyleSheet.app}>
-      <SearchHeader />
-      <VideoList videos={videos} />;
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
     </div>
   );
 }
